5995eeec1f45cca57593949a36ad60af9a6623c6
danyloy07 <iakimets.danil@gmail.com>
2023-11-07 22:39:06 +0200

    added for,while loops

--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,5 @@
 public class Variables {
-    public static void main(String [] args){
+    public static void main(string[] args){
         int num = 1;
         System.out.println(num);
         short num2 = 2;
