2f679b8d88f948cbe982dbcba83e3f3b7b684a7d
danyloy07 <iakimets.danil@gmail.com>
2023-11-07 12:14:46 +0200

    variables

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String [] args){
+        int num = 1;
+        System.out.println(num);
+        short num2 = 2;
+        System.out.println(num2);
+    }
+}
